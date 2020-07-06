d9bdf834a50e1561820ff67f759004271b760394
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-07-05 22:52:19 -0700

    finished all major elements, only thing left is history reload

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,29 @@
 $(document).ready(function () {
     var history = []
+    // checks if there is history in local storage
+    if (localStorage.getItem("history") != undefined) {
+        history = JSON.parse(localStorage.getItem("history"));
+
+        weatherCall5day(history[history.length - 1])
+        weatherCallCurrent(history[history.length - 1])
+
+
+    }
+    // this section runs the function to populate the history section 
+    populateHistory()
+    $("li").on("click", function () {
+        weatherCall5day(this.textContent)
+        weatherCallCurrent(this.textContent)
+    })
+
     $("#searchBtn").on("click", function (Event) {
         Event.preventDefault()
         var cityName = $(this).siblings()[0].value
 
-        // console.log(cityName)
         weatherCall5day(cityName)
         weatherCallCurrent(cityName)
+
+
     }
 
     )
@@ -17,18 +34,45 @@ $(document).ready(function () {
             url: queryURL,
             method: "GET",
         }).then(function (response) {
-            console.log(response)
+
+            var dates = $(".date")
+            // this section builds out the 5day forecast cards
+            for (var i = 0; i < dates.length; i++) {
+                var today = new Date();
+
+                var date = (today.getMonth() + 1) + '-' + (today.getDate() + (i + 1)) + '-' + today.getFullYear();
+                var dateDiv = $("#dayDate" + (i + 1))
+                var icon = response.list[i].weather[0].icon
+                dateDiv.text(date)
+                console.log(icon)
+                var newDiv = $("<div>")
+                var newTemp = $("<h6>")
+                var humDiv = $("<h6>")
+                var iconImg = $('<img />').attr('src', 'http://openweathermap.org/img/wn/' + icon + '.png').addClass('float-left')
+                newDiv.append(iconImg)
+                dateDiv.append(newDiv)
+                var tempVal = "Temp: " + response.list[i].main.temp
+                var humVal = "Humidity: " + response.list[i].main.humidity + "%"
+                newTemp.append(tempVal).addClass('float-left')
+                humDiv.append(humVal).addClass('float-left')
+                dateDiv.append(newTemp)
+                dateDiv.append(humDiv)
+
+
+
+            }
 
         })
     }
+    // this function gets uv data, checks the value for color coding and displays the value
     function uvIndex(lat, lon) {
-        var uvDiv = $("<h3>")
+
         var queryURL = "http://api.openweathermap.org/data/2.5/uvi?appid=98092cf02163d0e2417b3f3c5ba24af1&lat=" + lat + "&lon=" + lon
         $.ajax({
             url: queryURL,
             method: "GET",
         }).then(function (response) {
-            console.log(JSON.parse(response.value))
+
             var uvText = response.value;
             var uvDiv = $("#uv")
             uvDiv.html("UV Index: " + "<span id='uvValue'>" + uvText + "<span>")
@@ -48,13 +92,7 @@ $(document).ready(function () {
 
                 $("#uvValue").attr('style', 'background-color: red')
             }
-            // current.append(uvDiv)
 
-            // return current.append(uvDiv)
-
-
-            // .text("UV Index: " + uvVal)
-            // current.append(uv)
         })
     }
     // function for current weather 
@@ -66,21 +104,17 @@ $(document).ready(function () {
         }).then(function (response, err) {
             if (response) {
                 history.push(cityName)
+                localStorage.setItem("history", JSON.stringify(history))
                 console.log(history)
-
                 var lat = response.coord.lat
                 var lon = response.coord.lon
-                // console.log(lat)
-
-
-
                 // builds out current city title
                 var current = $("#current")
                 var currentTitle = $("#currentTitle")
                 var today = new Date();
-                var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+                var date = (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getFullYear();
                 var icon = (response.weather[0].icon)
-                console.log(icon)
+                // console.log(icon)
                 var iconImg = $('<img />').attr('src', "http://openweathermap.org/img/wn/" + icon + ".png")
                 currentTitle.text(response.name + " " + "(" + date + ")" + " ").append(iconImg)
 
@@ -96,35 +130,36 @@ $(document).ready(function () {
                 var windVal = response.wind.speed
                 var wind = $("#wind")
                 wind.text("Wind Speed: " + windVal + " mph")
+                //runs ajax call to get uv data
                 uvIndex(lat, lon)
-
-
-
-
-
-                // current.append(currentTitle)
-                // current.append(temp)
-                // current.append(Humid)
-                // current.append(wind)
                 current.append(uvDiv)
-
-
-
-
-
-
-
-
                 $("#currentContainer").html(current)
-
-                console.log(date)
-
             }
             else {
                 console.log(err)
             }
 
+
         })
+    }
+    function populateHistory() {
+        var list = $(".list")
+
+        for (i = 0; i < list.length; i++) {
+
+            if ((history[(history.length - i) - 1]) === undefined) {
+                list[i].append("")
+            } else {
+
+
+                list[i].append((history[(history.length - i) - 1]));
+
+
+
+
+
+            }
+        }
     }
 
 
